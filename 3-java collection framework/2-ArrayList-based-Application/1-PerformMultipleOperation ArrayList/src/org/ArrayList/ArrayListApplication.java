516c6236a06f6a7608715e13273bc76d0b7217e0
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2025-12-26 23:38:54 +0530

    List Collection ArrayList class on perform multiple operation's

--- 3-java collection framework/2-ArrayList-based-Application/1-PerformMultipleOperation ArrayList/src/org/ArrayList/ArrayListApplication.java
+++ 3-java collection framework/2-ArrayList-based-Application/1-PerformMultipleOperation ArrayList/src/org/ArrayList/ArrayListApplication.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class ArrayListApplication {
 
 	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
 //		Case 1: Add New element 
 //		Case 2: View all elements
 //		Case 3: Search element in ArrayList
@@ -10,7 +11,19 @@ public class ArrayListApplication {
 //		Case 5: check the size of ArrayList 
 //		Case 6: retrieve data from ArrayList 
 		
-		Vector v=new Vector();
+		ArrayList al=new ArrayList();
+		
+		do {
+			System.out.println("1.Add new element ");
+			System.out.println("2.View All elements");
+			System.out.println("3.Search element in ArrayList");
+			System.out.println("4.Delete element from ArrayList");
+			System.out.println("5.Check the size of ArrayList");
+			System.out.println("6.Retrive data from ArrayList");
+			
+			int choice =sc.nextInt();
+			
+		} while (true);
 		
 	}
 
