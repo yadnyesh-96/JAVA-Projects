b4201eccba40d0eaf6d01a7a079a108a4f748d84
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2025-12-26 22:44:27 +0530

    POJO class Example with Vector collection to store differenet type in collection

--- 3-java collection framework/1-Vector-based-Application/2-ProductListApp/src/org/product/Product.java
+++ 3-java collection framework/1-Vector-based-Application/2-ProductListApp/src/org/product/Product.java
@@ -1,5 +1,14 @@
 package org.product;
 
 public class Product {
-
+	private int id;
+	private String name;
+	private double price;
+	
+	Product(int id,String name,double price){
+		this.id=id;
+		this.name=name;
+		this.price=price;
+	}
+	
 }
--- 3-java collection framework/1-Vector-based-Application/2-ProductListApp/src/org/product/ProductsApplication.java
+++ 3-java collection framework/1-Vector-based-Application/2-ProductListApp/src/org/product/ProductsApplication.java
@@ -1,5 +1,9 @@
 package org.product;
-
+import java.util.*;
 public class ProductsApplication {
-
+	public static void main(String x[]) {
+		Vector v=new Vector();
+		
+		
+	}
 }
