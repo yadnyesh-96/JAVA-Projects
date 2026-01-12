b401ac6b49b3b7e7f70ade896a8035c2afbd9c36
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-12 16:50:27 +0530

    Employee data manage by vector collection

--- 3-java collection framework/1-Vector-based-Application/1-EmployeeApp/src/employee/org/EmployeeApplication.java
+++ 3-java collection framework/1-Vector-based-Application/1-EmployeeApp/src/employee/org/EmployeeApplication.java
@@ -7,7 +7,12 @@ public class EmployeeApplication {
 		
 		Vector v=new Vector();
 		
-		
+		Employee e1=new Employee(1,"ABC",15000);
+		Employee e2=new Employee(2,"MNO",18000);
+		Employee e3=new Employee(3,"PQR",17500);
+		Employee e4=new Employee(4,"STU",15260);
+		Employee e5=new Employee(5,"VWX",12470);
+		Employee e6=new Employee(6,"OOP",11290);
 		
 		v.add(e1);
 		v.add(e2);
