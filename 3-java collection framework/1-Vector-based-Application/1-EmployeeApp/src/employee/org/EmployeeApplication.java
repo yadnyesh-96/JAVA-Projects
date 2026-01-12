c3660a52f801969ffdaac47bf48f231fb9b02da6
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-12 16:51:01 +0530

    Employee data manage by vector collection

--- 3-java collection framework/1-Vector-based-Application/1-EmployeeApp/src/employee/org/EmployeeApplication.java
+++ 3-java collection framework/1-Vector-based-Application/1-EmployeeApp/src/employee/org/EmployeeApplication.java
@@ -24,7 +24,10 @@ public class EmployeeApplication {
 		System.out.print("ID\tName\tSalary\n");
 		Iterator i=v.iterator();
 		
-		
+		while(i.hasNext()) {
+			Object obj=i.next();
+			Employee e=(Employee)obj;
+			System.out.print(e.getId()+"\t"+e.getName()+"\t"+e.getSalary()+"\n");
 		}
 		
 
