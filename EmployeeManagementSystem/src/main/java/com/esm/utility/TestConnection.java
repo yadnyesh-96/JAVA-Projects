5588bc6388496e7616f7cfd2368b6a2f6fb93f54
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-06-08 00:16:09 +0530

    Employee Database Management System Repository class

--- EmployeeManagementSystem/src/main/java/com/esm/utility/TestConnection.java
+++ EmployeeManagementSystem/src/main/java/com/esm/utility/TestConnection.java
@@ -1,18 +1,66 @@
 package com.esm.utility;
 
-import java.sql.Connection;
+import java.util.List;
+
+import com.ems.repository.DepartmentRepository;
+import com.esm.model.Department;
+import com.esm.repository.impl.DepartmentRepositoryImpl;
 
 public class TestConnection {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Connection conn = DBConnection.getConnection();
-		
-		if(conn != null) {
-			System.out.println("Connection Success");
-		}else {
-			System.out.println("Connection Failed");
+
+		DepartmentRepository repo = new DepartmentRepositoryImpl();
+
+//		testAddDepartment(repo);
+//		testGetAllDepartment(repo);
+//		testGetDeptById(repo);
+//		testUpdateDepartment(repo);
+		testDeleteDep(repo);
+
+	}
+
+	public static void testAddDepartment(DepartmentRepository repo) {
+		Department dep = new Department();
+
+		dep.setDeptName("Support");
+		dep.setDescription("Customer Support Team");
+
+		boolean res = repo.addDepartment(dep);
+
+		System.out.println("Department Added : " + res);
+	}
+
+	public static void testGetAllDepartment(DepartmentRepository repo) {
+
+		List<Department> list = repo.getAllDepartments();
+
+		for (Department dep : list) {
+			System.out.println(dep.getDeptId() + "\t" + dep.getDeptName() + "\t" + dep.getDescription());
 		}
 	}
 
+	public static void testGetDeptById(DepartmentRepository repo) {
+		Department dep = repo.getDepartmentById(1);
+		System.out.println(dep.getDeptId() + "\t" + dep.getDeptName() + "\t" + dep.getDescription());
+
+	}
+
+	public static void testUpdateDepartment(DepartmentRepository repo) {
+		Department dep = repo.getDepartmentById(1);
+
+		dep.setDeptName("Human Resource");
+		dep.setDescription("Updated HR department");
+
+		boolean res = repo.updateDepartment(dep);
+
+		System.out.println("Updated: " + res);
+	}
+
+	public static void testDeleteDep(DepartmentRepository repo) {
+
+		boolean res = repo.deleteDepartment(5);
+		System.out.println("Deleted : " + res);
+
+	}
 }
