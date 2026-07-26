8f4a45e92f0d0ef47059b7c8b9cb651601bf8bca
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-07-27 01:39:35 +0530

    Online Exam Portal

--- OnlineExamPortal/src/main/java/com/oem/repository/SubjectRepository.java
+++ OnlineExamPortal/src/main/java/com/oem/repository/SubjectRepository.java
@@ -5,8 +5,14 @@ import java.util.List;
 import com.oem.model.Subject;
 
 public interface SubjectRepository {
-	
+
 	public boolean addSubject(Subject sub);
-	
+
 	public List<Subject> getAllSubjects();
+
+	public boolean deleteSubject(int subId);
+
+	public Subject getSubject(Subject sub);
+
+	public boolean updateSubject(Subject sub);
 }
