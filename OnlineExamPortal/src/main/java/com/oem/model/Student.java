a906b760046d20b46f465785fb6ec9038dd1fba3
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-08-01 00:32:52 +0530

    servlet online exam portal

---
+++ OnlineExamPortal/src/main/java/com/oem/model/Student.java
@@ -0,0 +1,14 @@
+package com.oem.model;
+
+public class Student {
+	private int stdId;
+	private String StudentID;
+	private String fullName;
+	private String email;
+	private String mobileNumber;
+	private String country;
+	private String DOB;
+	private String address;
+	private String username;
+	private String password;
+}
